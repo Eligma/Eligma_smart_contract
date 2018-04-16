e5c37abf73e3dd44789556b02d8a16d6387a9670
Eligma <37958427+Eligma@users.noreply.github.com>
2018-04-16 21:15:23 +0200

    Add files via upload

---
+++ Utils/ReentrancyHandlingContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.21;
+
+contract ReentrancyHandlingContract{
+
+    bool locked;
+
+    modifier noReentrancy() {
+        require(!locked);
+        locked = true;
+        _;
+        locked = false;
+    }
+}
